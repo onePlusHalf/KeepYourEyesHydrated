c6df7db9564ac1198f13dd1a679d98dd458c75c1
Rahul <kumarrahul@johndeere.com>
2020-06-23 23:11:40 +0530

    initial commit

---
+++ src/main/java/com/uncanny/eyecare/KeepYourEyesHydratedApplication.java
@@ -0,0 +1,13 @@
+package com.uncanny.eyecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepYourEyesHydratedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepYourEyesHydratedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uncanny/eyecare/KeepYourEyesHydratedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uncanny.eyecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepYourEyesHydratedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
