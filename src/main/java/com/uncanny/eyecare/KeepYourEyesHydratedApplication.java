0df413340234f6f8d565b7325f053f06da796e3b
Rahul <kumarrahul@johndeere.com>
2020-06-24 13:43:00 +0530

    remove tray icons and set time to 20 minutes

--- src/main/java/com/uncanny/eyecare/KeepYourEyesHydratedApplication.java
+++ src/main/java/com/uncanny/eyecare/KeepYourEyesHydratedApplication.java
@@ -6,6 +6,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @EnableScheduling
@@ -14,26 +16,40 @@ public class KeepYourEyesHydratedApplication {
         System.setProperty("java.awt.headless", "false");
     }
 
+    private List<TrayIcon> trayList = new ArrayList<>();
     public static void main(String[] args) {
         SpringApplication.run(KeepYourEyesHydratedApplication.class, args);
     }
 
-    @Scheduled(fixedRate = 50000)
+    @Scheduled(fixedRate = 20*60*1000)
     public void scheduledMethod() throws AWTException {
+        String toolTip = "Keep Eyes Hydrated";
+
         if (SystemTray.isSupported()) {
             SystemTray tray = SystemTray.getSystemTray();
+            for (TrayIcon trayIcon : trayList){
+                tray.remove(trayIcon);
+            }
+
+            TrayIcon[] trayIcons = tray.getTrayIcons();
+
+            for (TrayIcon trayIcon: trayIcons ) {
+                if (trayIcon.getToolTip().equals(toolTip)) {
+                    tray.remove(trayIcon);
+                }
+            }
 
             //If the icon is a file
             Image image = Toolkit.getDefaultToolkit().createImage("2_1.jpg");
             //Alternative (if the icon is on the classpath):
             //Image image = Toolkit.getDefaultToolkit().createImage(getClass().getResource("icon.png"));
 
-            TrayIcon trayIcon = new TrayIcon(image, "Keep Eyes Hydrated");
+            TrayIcon trayIcon = new TrayIcon(image, toolTip);
             //Let the system resize the image if needed
             trayIcon.setImageAutoSize(true);
             //Set tooltip text for the tray icon
-            trayIcon.setToolTip("Keep Eyes Hydrated");
             tray.add(trayIcon);
+            trayList.add(trayIcon);
 
             trayIcon.displayMessage("Hey, Man", "Do not forget to look around you.", TrayIcon.MessageType.INFO);
         } else
